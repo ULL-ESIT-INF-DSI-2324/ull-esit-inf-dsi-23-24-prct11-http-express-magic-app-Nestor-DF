ff91e5a6d72fa3b7c0a8824959309511290b86c7
Néstor Delgado <alu0101488998@ull.edu.es>
2024-04-11 21:06:58 +0000

    .

--- tests/formatCard.spec.ts
+++ tests/formatCard.spec.ts
@@ -1,79 +1,34 @@
 import 'mocha';
 import { expect } from 'chai';
-import { formatCardString, colorCard } from '../src/formatCards.js';
+import { JSONtoCard } from '../src/formatCards.js';
+import { MagiCard, Color, Type, Rarity } from '../src/MagiCard.js';
 
-describe('formatCardString', () => {
-  it('should format the card string correctly', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Blue", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      'ID: 1\nName: Card Name\nMana cost: 2\nColor: Blue\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n';
-    const result = formatCardString(card);
-    expect(result).to.be.equal(expectedOutput);
-  });
-});
+describe('JSONtoCard', () => {
+  it('should convert a card from JSON to a MagiCard object', () => {
+    const card = {
+      id: 4,
+      name: 'Qiyana',
+      manaCost: 969696,
+      color: 'White',
+      cardType: 'Artifact',
+      rarity: 'Rare',
+      rulesText: 'Tap: kkk',
+      marketValue: 80,
+    };
 
-describe('colorCard', () => {
-  it('should color the card string correctly for Blue color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Blue", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[34;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: Blue\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
-  });
+    const magiCard = new MagiCard(
+      card.id,
+      card.name,
+      card.manaCost,
+      card.color as Color,
+      card.cardType as Type,
+      card.rarity as Rarity,
+      card.rulesText,
+      card.marketValue,
+    );
 
-  it('should color the card string correctly for Red color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Red", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[31;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: Red\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
-  });
+    const result = JSONtoCard(card);
 
-  it('should color the card string correctly for Green color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Green", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[32;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: Green\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
-  });
-
-  it('should color the card string correctly for White color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "White", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[37;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: White\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
-  });
-
-  it('should color the card string correctly for Black color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Black", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[30;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: Black\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
-  });
-
-  it('should color the card string correctly for Colorless color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Colorless", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[30;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: Colorless\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
-  });
-
-  it('should color the card string correctly for Multicolor color', () => {
-    const card =
-      '{"id": 1, "name": "Card Name", "manaCost": 2, "color": "Multicolor", "type": "Creature", "rarity": "Common", "rulesText": "Some rules text", "marketValue": 10, "powerAndToughness": "2/2"}';
-    const expectedOutput =
-      '\u001b[30;1;3mID: 1\nName: Card Name\nMana cost: 2\nColor: Multicolor\nType: Creature\nRarity: Common\nRules text: Some rules text\nMarket value: 10\nPower/Toughness: 2/2\n\u001b[0m';
-    const result = colorCard(card);
-    expect(result).to.be.equal(result);
+    expect(result).to.deep.equal(magiCard);
   });
 });
